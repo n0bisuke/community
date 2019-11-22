a1bbb8055a8fe09d4425b285f6bfcc1924f695e0
のびすけ <rsksound@gmail.com>
2019-11-22 21:18:33 +0900

    deploy

---
+++ .github/gitpush.sh
@@ -0,0 +1,21 @@
+git config --global user.name "n0bisuke"
+git config --global user.email "n0bisuke@gmail.com"
+
+git remote set-url origin https://n0bisuke:${GITHUB_TOKEN}@github.com/n0bisuke/community.git
+
+git checkout -b master
+git branch -a
+
+git log -1
+last_commit_message="$(git log -1 | tail -1)"
+echo $last_commit_message
+
+if [ -z "$(echo $last_commit_message | grep updater)" ]; then
+    git log -1 | head -1 > last_commit.txt
+    cat last_commit.txt
+    git add last_commit.txt
+    git commit -m '[updater] update last commit'
+    git push origin HEAD
+else
+    echo "nothing updated"
+fi
