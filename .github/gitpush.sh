82ed7c938354772f4b71ce6fd1bc8b9e6c63f261
のびすけ <rsksound@gmail.com>
2019-11-23 13:39:06 +0900

    gh actions

--- .github/gitpush.sh
+++ .github/gitpush.sh
@@ -1,8 +1,10 @@
-git config --global user.name "n0bisuke"
-git config --global user.email "rsksound@gmail.com"
+#!/bin/bash
 
-git remote set-url origin https://${GITHUB_TOKEN}@github.com/n0bisuke/community.git
-# git remote set-url origin https://n0bisuke:${GITHUB_TOKEN}@github.com/n0bisuke/community.git
+git config --global user.name "n0bisuke"
+git config --global user.email "aki.bdash@gmail.com"
+
+git remote set-url origin https://n0bisuke:${GITHUB_TOKEN}@github.com/n0bisuke/community.git
+
 git checkout -b master
 git branch -a
 
@@ -11,9 +13,9 @@ last_commit_message="$(git log -1 | tail -1)"
 echo $last_commit_message
 
 if [ -z "$(echo $last_commit_message | grep updater)" ]; then
-    git log -1 | head -1 > last_commit.txt
-    cat last_commit.txt
-    git add last_commit.txt
+    # git log -1 | head -1 > last_commit.txt
+    # cat last_commit.txt
+    git add -A
     git commit -m '[updater] update last commit'
     git push origin HEAD
 else
